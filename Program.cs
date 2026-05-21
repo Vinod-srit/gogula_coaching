49307875ad8e54a47f5ba3ab1d7d1d9770b888ae
Vinod <gogulavinod2002@gmail.com>
2026-05-21 23:12:45 +0530

    updated website

--- Program.cs
+++ Program.cs
@@ -16,6 +16,7 @@ app.MapPost("/saveStudent", async (HttpContext context) =>
     string fname = form["fname"].ToString();
     string lname = form["lname"].ToString();
     string year_of_passout = form["year_of_passout"].ToString();
+    string course = form["course"].ToString();
     string mobile_number = form["mobile_number"].ToString();
     string graduation = form["graduation"].ToString();
     string address = form["address"].ToString();
@@ -35,6 +36,7 @@ app.MapPost("/saveStudent", async (HttpContext context) =>
         cmd.Parameters.AddWithValue("@fname", fname);
         cmd.Parameters.AddWithValue("@lname", lname);
         cmd.Parameters.AddWithValue("@year_of_passout", year_of_passout);
+        cmd.Parameters.AddWithValue("@course",course);
         cmd.Parameters.AddWithValue("@mobile_number", mobile_number);
         cmd.Parameters.AddWithValue("@graduation", graduation);
         cmd.Parameters.AddWithValue("@address", address);
