b154860f22e172f0509f16a3ee17ca07c159ef31
Vinod <gogulavinod2002@gmail.com>
2026-05-21 22:22:44 +0530

    first commit

---
+++ Pages/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Gogula_Coaching.Pages;
+
+public class IndexModel : PageModel
+{
+    public void OnGet()
+    {
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,49 @@
+using Microsoft.AspNetCore.Http;
+using MySql.Data.MySqlClient;
+
+var builder = WebApplication.CreateBuilder(args);
+var app = builder.Build();
+
+app.UseDefaultFiles();
+app.UseStaticFiles();
+
+string connectionString = "server=localhost;database=coachingcenter;user=root;password=Vinod@2002;";
+
+app.MapPost("/saveStudent", async (HttpContext context) =>
+{
+    var form = await context.Request.ReadFormAsync();
+
+    string fname = form["fname"].ToString();
+    string lname = form["lname"].ToString();
+    string year_of_passout = form["year_of_passout"].ToString();
+    string mobile_number = form["mobile_number"].ToString();
+    string graduation = form["graduation"].ToString();
+    string address = form["address"].ToString();
+    string reference = form["reference"].ToString();
+
+    using (MySqlConnection con = new MySqlConnection(connectionString))
+    {
+        con.Open();
+
+        string query = @"insert into students
+        (fname,lname,year_of_passout,mobile_number,graduation,address,reference)
+        values
+        (@fname,@lname,@year_of_passout,@mobile_number,@graduation,@address,@reference)";
+
+        MySqlCommand cmd = new MySqlCommand(query, con);
+
+        cmd.Parameters.AddWithValue("@fname", fname);
+        cmd.Parameters.AddWithValue("@lname", lname);
+        cmd.Parameters.AddWithValue("@year_of_passout", year_of_passout);
+        cmd.Parameters.AddWithValue("@mobile_number", mobile_number);
+        cmd.Parameters.AddWithValue("@graduation", graduation);
+        cmd.Parameters.AddWithValue("@address", address);
+        cmd.Parameters.AddWithValue("@reference", reference);
+
+        cmd.ExecuteNonQuery();
+    }
+
+    context.Response.Redirect("/");
+});
+
+app.Run();
